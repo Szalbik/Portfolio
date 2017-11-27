cf8e7c1eba8ea78fb007d12e7d5834f132b7e09c
DamianSzalbierz <szalbierz.d.k@gmail.com>
2017-11-27 14:32:35 +0100

    Add eslint and react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Redirect, Route, Link, Switch } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import ContactPage from './components/pages/ContactPage';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Link to="/home">Home</Link>
+    <Link to="/contact">Contact</Link>
+
+    <Switch>
+      <Route path="/home" component={HomePage} />
+      <Route path="/contact" component={ContactPage} />
+      <Redirect to="/home" />
+    </Switch>
+  </div>
+);
 
 export default App;
