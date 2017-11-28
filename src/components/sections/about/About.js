6f3e671ea41e227ef2bbf15f18131e6bb0927be0
DamianSzalbierz <szalbierz.d.k@gmail.com>
2017-11-28 16:27:43 +0100

    Add info about me

--- src/components/sections/about/About.js
+++ src/components/sections/about/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, List } from 'semantic-ui-react';
 import SectionTitle from '../../section-title/SectionTitle';
 import './About.scss';
 
@@ -12,7 +12,76 @@ class About extends React.Component {
         <SectionTitle text="about me" />
         <Grid columns={2}>
           <Grid.Row>
-            <Grid.Column width={4} />
+            <Grid.Column width={4}>
+              <List animated>
+                <List.Item>
+                  <List.Icon name="marker" />
+                  <List.Content>Gorzów Wielkopolski</List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Icon name="phone square" />
+                  <List.Content>667 768 745</List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Icon name="mail" />
+                  <List.Content>
+                    <a href="mailto:szalbierz.d.k@gmail.com">
+                      szalbierz.d.k@gmail.com
+                    </a>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Icon name="linkify" />
+                  <List.Content>
+                    <a href="http://www.damianszalbierz.pl/">My website</a>
+                  </List.Content>
+                </List.Item>
+              </List>
+              <List horizontal>
+                <List.Item>
+                  <List.Content>
+                    <a
+                      href="https://www.github.com/Szalbik"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <List.Icon size="large" name="github" />
+                    </a>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <a
+                    href="https://bitbucket.org/DamianSzalbierz/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <List.Icon size="large" name="bitbucket" />
+                  </a>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <a
+                      href="https://www.facebook.com/damian.szalbierz.3"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <List.Icon size="large" name="facebook official" />
+                    </a>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <a
+                      href="https://www.linkedin.com/in/damian-szalbierz-04110a110/"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      <List.Icon size="large" name="linkedin square" />
+                    </a>
+                  </List.Content>
+                </List.Item>
+              </List>
+            </Grid.Column>
 
             <Grid.Column width={12} />
           </Grid.Row>
