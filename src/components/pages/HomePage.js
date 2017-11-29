4d27a8124db2df457a3bee43a5d1e7b20ca0d286
DamianSzalbierz <szalbierz.d.k@gmail.com>
2017-11-29 18:42:22 +0100

    refactor post in experience to HeaderWithText and fast create education

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,6 +4,7 @@ import Body from '../sections/body/Body';
 import About from '../sections/about/About';
 import Skills from '../sections/skills/Skills';
 import Experience from '../sections/experience/Experience';
+import Education from '../sections/education/Education';
 
 class HomePage extends React.Component {
   state = {};
@@ -18,6 +19,7 @@ class HomePage extends React.Component {
         </Body>
         <Body color="#2f4152">
           <Experience />
+          <Education />
         </Body>
       </div>
     );
