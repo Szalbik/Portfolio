9770705f3c9730a69c4610ad3fe18b3426b4c461
DamianSzalbierz <szalbierz.d.k@gmail.com>
2017-11-29 01:30:44 +0100

    Create experience - there's need to be refactor!!

--- src/components/sections/experience/Experience.js
+++ src/components/sections/experience/Experience.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Header, Label, Segment, Icon } from 'semantic-ui-react';
 import SectionTitle from '../../section-title/SectionTitle';
+import './Experience.scss';
 
 class Experience extends React.Component {
   state = {};
@@ -8,6 +10,38 @@ class Experience extends React.Component {
     return (
       <div className="experience">
         <SectionTitle text="experience" round />
+
+        <Segment raised>
+          <Header as="h2">
+            <Icon name="circle thin" />
+            <Header.Content>
+              Pierwsza praca
+              <Label horizontal>1994 - 2000</Label>
+            </Header.Content>
+            <Header.Subheader>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Header.Subheader>
+          </Header>
+        </Segment>
+
+        <Segment raised>
+          <Header as="h2">
+            <Icon name="circle thin" />
+            <Header.Content>
+              Pierwsza praca
+              <Label horizontal>1994 - 2000</Label>
+            </Header.Content>
+            <Header.Subheader>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Header.Subheader>
+          </Header>
+        </Segment>
       </div>
     );
   }
