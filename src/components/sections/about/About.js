47c6f91d5ea51b4883e889999d53cd08b35dbc6a
DamianSzalbierz <szalbierz.d.k@gmail.com>
2017-12-04 15:08:03 +0100

    Add profile img

--- src/components/sections/about/About.js
+++ src/components/sections/about/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Grid, List, Image } from 'semantic-ui-react';
 import SectionTitle from '../../section-title/SectionTitle';
 import './About.scss';
-import img from '../../../images/image.png';
+import img from '../../../images/profile.gif';
 import cv from '../../../files/Curriculum Vitae.pdf';
 
 class About extends React.Component {
@@ -17,11 +17,19 @@ class About extends React.Component {
             <Grid.Column computer={4} tablet={5} only="computer tablet">
               <List animated>
                 <List.Item>
-                  <List.Icon name="marker" />
-                  <List.Content>Gorzów Wielkopolski</List.Content>
+                  <List.Icon size="large" name="marker" />
+                  <List.Content>
+                    <a
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href="https://www.google.pl/maps/place/Gorz%C3%B3w+Wielkopolski/@52.7312454,15.0781532,11z/data=!3m1!4b1!4m5!3m4!1s0x47071f0a5a4118b7:0x5dd10df3cb5799dd!8m2!3d52.7325285!4d15.2369305"
+                    >
+                      Gorzów Wielkopolski
+                    </a>
+                  </List.Content>
                 </List.Item>
                 <List.Item>
-                  <List.Icon name="github" />
+                  <List.Icon size="large" name="github" />
                   <List.Content>
                     <a
                       href="https://www.github.com/Szalbik"
@@ -33,7 +41,7 @@ class About extends React.Component {
                   </List.Content>
                 </List.Item>
                 <List.Item>
-                  <List.Icon name="bitbucket" />
+                  <List.Icon size="large" name="bitbucket" />
                   <List.Content>
                     <a
                       href="https://bitbucket.org/DamianSzalbierz/"
@@ -45,45 +53,22 @@ class About extends React.Component {
                   </List.Content>
                 </List.Item>
                 <List.Item>
-                  <List.Icon name="cloud download" />
+                  <List.Icon size="large" name="cloud download" />
                   <List.Content>
                     <a href={cv} download="Curriculum Vitae - Damian Szalbierz">
                       Curriculum Vitae
                     </a>
                   </List.Content>
                 </List.Item>
-              </List>
-              <List horizontal>
                 <List.Item>
-                  <List.Content>
-                    <a
-                      href="https://www.facebook.com/damian.szalbierz.3"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      <List.Icon size="large" name="facebook official" />
-                    </a>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
+                  <List.Icon size="large" name="linkedin square" />
                   <List.Content>
                     <a
                       href="https://www.linkedin.com/in/damian-szalbierz-04110a110/"
                       rel="noopener noreferrer"
                       target="_blank"
                     >
-                      <List.Icon size="large" name="linkedin square" />
-                    </a>
-                  </List.Content>
-                </List.Item>
-                <List.Item>
-                  <List.Content>
-                    <a
-                      href="https://twitter.com/SzalbierzDK"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      <List.Icon size="large" name="twitter" />
+                      Linkedin
                     </a>
                   </List.Content>
                 </List.Item>
